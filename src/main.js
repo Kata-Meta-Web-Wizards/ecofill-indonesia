ddd7c4d93fb61446e55ba3fc13114d6445272f4d
namarabigail <bigelnp10@gmail.com>
2026-07-06 23:39:49 +0700

    feat(core): integrate calculator section into main entry

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
+import { initCalculator } from './sections/calculator/calculator.js'
 import "./styles/main.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
+initCalculator()
 AOS.init({ duration: 700, easing: "ease-out-cubic", once: true });
