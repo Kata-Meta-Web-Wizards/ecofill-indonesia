36e6325690512c8cd88164afd419297c8e4e3618
namarabigail <bigelnp10@gmail.com>
2026-07-07 15:50:44 +0700

    feat: wire up map section in main.js and index.html

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import { initCalculator } from './sections/calculator/calculator.js'
 import "./styles/main.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-initCalculator()
+import { initCalculator } from './sections/calculator/calculator.js'
+import { initMap } from "./sections/map/map.js";
+
 AOS.init({ duration: 700, easing: "ease-out-cubic", once: true });
+
+initCalculator()
+initMap();
