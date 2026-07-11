53da87a640980c2f4ac898df1d0f156901c707f5
unknown <fini.ardiyanti@gmail.com>
2026-07-11 13:27:24 +0700

    fix: resolve merge conflict, urutan section

--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
+import { initNavbar } from './sections/navbar/navbar.js'
+import { initHero } from './sections/hero/hero.js'
+import { initComposition } from './sections/composition/composition.js'
+import { initJourney } from './sections/journey/journey.js'
+import { initImpact } from './sections/impact/impact.js'
+import { initCalculator } from './sections/calculator/calculator.js'
+import { initMap } from './sections/map/map.js'
+import { initChallenge } from './sections/challenge/challenge.js'
+import { initFaq } from './sections/faq/faq.js'
 import "./styles/main.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-import { initCalculator } from './sections/calculator/calculator.js'
-import { initMap } from "./sections/map/map.js";
-import { initChallenge } from "./sections/challenge/challenge.js";
-import { initFaq } from "./sections/faq/faq.js";
+initNavbar()
+initHero()
+initComposition()
+initJourney()
+initImpact()
+initCalculator()
+initMap()
+initChallenge()
+initFaq()
 
 AOS.init({ duration: 700, easing: "ease-out-cubic", once: true });
-
-initCalculator()
-initMap();
-initChallenge();
-initFaq();
